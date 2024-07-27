a6471d467f38ef886716c068d32cc5e2205b02d5
wangtl175 <wangtlong3@mail2.sysu.edu.cn>
2024-07-27 20:49:28 +0800

    lesson 3: 修复空白处

--- main.cpp
+++ main.cpp
@@ -72,39 +72,39 @@ bool is_in_triangle(const Vec2i (&pts)[3], const Vec2i &P) {
     return true;
 }
 
-Vec3f barycentric(const Vec3f (&pts)[3], const Vec3f &P) {
+Vec3f barycentric(const Vec3i (&pts)[3], const Vec3i &P) {
     Vec3f u = Vec3f(pts[2].x - pts[0].x, pts[1].x - pts[0].x, pts[0].x - P.x) ^
               Vec3f(pts[2].y - pts[0].y, pts[1].y - pts[0].y, pts[0].y - P.y);
     /* `pts` and `P` has integer value as coordinates, that is, u has integer value as coordinates
        so `abs(u[2])` < 1 means `u[2]` is 0, that means
        triangle is degenerate, in this case return something with negative coordinates */
-    if (std::abs(u.z) < 1e-5) return {-1, 1, 1};
+    if (std::abs(u.z) < 1) return {-1, 1, 1};
     return {1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z};
 }
 
-void triangle(Vec3f (&pts)[3], float *z_buffer, TGAImage &image, const TGAColor &color) {
-    Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
-    Vec2f clamp(image.width() - 1, image.height() - 1);
+void triangle(Vec3i (&pts)[3], float *z_buffer, TGAImage &image, const TGAColor &color) {
+    Vec2i bboxmin(image.width() - 1, image.height() - 1);
+    Vec2i bboxmax(0, 0);
+    Vec2i clamp(image.width() - 1, image.height() - 1);
 
     for (int i = 0; i < 3; ++i) {
-        bboxmin.x = std::max(0.f, std::min(bboxmin.x, pts[i].x));
-        bboxmin.y = std::max(0.f, std::min(bboxmin.y, pts[i].y));
+        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
 
         bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
         bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
     }
 
-    Vec3f P;
+    Vec3i P;
     for (P.x = bboxmin.x; P.x <= bboxmax.x; ++P.x) {
         for (P.y = bboxmin.y; P.y <= bboxmax.y; ++P.y) {
             Vec3f bc_screen = barycentric(pts, P);  // 重心坐标
             if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-            P.z = pts[0].z * bc_screen.x + pts[1].z * bc_screen.y + pts[2].z * bc_screen.z;
 
-            int idx = int(P.x + P.y * WIDTH);
-            if (z_buffer[idx] < P.z) {
-                z_buffer[idx] = P.z;
+            float Pz = pts[0].z * bc_screen.x + pts[1].z * bc_screen.y + pts[2].z * bc_screen.z;
+            int idx = P.x + P.y * WIDTH;
+            if (z_buffer[idx] < Pz) {
+                z_buffer[idx] = Pz;
                 image.set(P.x, P.y, color);
             }
         }
@@ -130,7 +130,7 @@ int main(int argc, char **argv) {
         std::vector<int> face = model->face(i);
 
         Vec3f world_coords[3];
-        Vec3f screen_coords[3];
+        Vec3i screen_coords[3];
         for (int j = 0; j < 3; ++j) {
             world_coords[j] = model->vert(face[j]);
 
